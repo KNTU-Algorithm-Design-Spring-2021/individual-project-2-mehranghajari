1a736a3e668777e5ad9457db990243265b65a384
Mehran Ghajari <mehranghajari42@live.com>
2021-04-10 04:16:31 +0430

    P2

---
+++ P2/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	dict map[string]int
+)
+
+func start(sentence string) []string{
+	dp := make( map[string][]string)
+	return wordBreak(sentence, dp)
+}
+func wordBreak(sentence string, dp map[string][]string) []string{
+
+	if len(dp[sentence]) != 0{
+		return dp[sentence]
+	}
+	var result []string
+	if dict[sentence] == 1 {
+		result = append(result, sentence)
+}
+
+	for i := 1 ; i< len(sentence); i++ {
+		prefix := sentence[0: i ]
+		if dict[prefix] == 1 {
+			returnStringsList := wordBreak(sentence[i:], dp)
+
+			for _, v :=range returnStringsList {
+				result = append(result, prefix + " " + v)
+			}
+		}
+	}
+	dp[sentence] = result
+	return result
+}
+
+func main() {
+	filePath := "dict.txt"
+	readFile , err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Failed to read File")
+	}
+	f  := bufio.NewScanner(readFile)
+
+	f.Split(bufio.ScanWords)
+	dict = make(map[string]int)
+	for f.Scan() {
+		dict[f.Text()] = 1
+	}
+	sentence := "Whenthemadnessallaroundusstartstotakeit'stollTakeatripinsidemyheadeverybody's"
+	for _, v := range start(sentence) {
+		fmt.Println(v)
+	}
+
+}
